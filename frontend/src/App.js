43a27fb99d5d8dd9d7116d4890b003e0d0cc7f58
santiclfriedrich <santiclear1@hotmail.com>
2022-10-18 17:53:02 -0300

    Listado de productos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 
 function App() {
@@ -7,7 +8,23 @@ function App() {
         <a href="/">pernambuco</a>
       </header>
       <main>
-        list products
+        <h1>Pernam Favourites</h1>
+        <div className="products">
+        {data.products.map(product => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-info">
+            <a href={`/product/${product.slug}`}>
+            <p> {product.name} </p>
+            </a>
+            <p> <strong> {product.price} </strong> </p>
+            <button>Add to cart</button>
+            </div>
+          </div>
+          ))}
+          </div>
       </main>
     </div>
   );
