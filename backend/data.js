7c1b4fa81ab8b64f6673672d5fb5d3be312bc7d6
santiclfriedrich <santiclear1@hotmail.com>
2022-10-20 00:26:51 -0300

    Implementacion de Node Server

---
+++ backend/data.js
@@ -0,0 +1,36 @@
+const data = {
+    products: [
+        {
+            name: 'Remera Negra',
+            slug: 'remera-negra-pernam',
+            category: 'Remeras',
+            image: '/images/remera-black.png',
+            price: 3700,
+            countInStock: 7,
+            brand: 'Pernambuco',
+            description: 'Remera negra lisa 100% algodon'
+        },
+        {
+            name: 'Gorra Marron',
+            slug: 'gorra-marron-ejx',
+            category: 'Gorras',
+            image: '/images/gorra-marron.png',
+            price: 2500,
+            countInStock: 5,
+            brand: 'Ejx',
+            description: 'Gorra Corderoy basta media 100% algodón. Talle único. Etiqueta de marca bordada en frente.'
+        },
+        { 
+            name: 'Cargo Pants',
+            slug: 'cargo-azul-pernam',
+            category: 'Pantalones',
+            image: '/images/cargo-pants.png',
+            price: 6300,
+            countInStock: 2,
+            brand: 'Pernambuco',
+            description: 'Pantalon Cargo Azul. Tiro alto/ Regular. Composición: Algodón'
+        },
+    ],
+};
+
+export default data;
