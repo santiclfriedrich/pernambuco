1d0d4a74e69f77b8ace80cd835e493319f33dafb
santiclfriedrich <santiclear1@hotmail.com>
2022-10-26 17:15:50 -0300

    Pantalla 'Cart' Agregada

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,20 +3,21 @@ import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
 function Product(props) {
-    const {product } = props;
-    return (
+  const { product } = props;
+  return (
     <Card>
-    <Link to={`/product/${product.slug}`}>
-    <img src={product.image} className='card-img-top' alt={product.name} />
-    </Link>
-    <Card.Body>
-        <Link to={`/product/${product.slug}`}>
-        <Card.Title> {product.name} </Card.Title>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
       </Link>
-      <Card.Text>${product.price}</Card.Text>
-      <Button>Add to cart</Button>
-    </Card.Body>
-  </Card>)
-};
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title> {product.name} </Card.Title>
+        </Link>
+        <Card.Text>${product.price}</Card.Text>
+        <Button>Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+}
 
 export default Product;
---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,95 @@
+import { useContext } from 'react';
+import Col from 'react-bootstrap/Col';
+import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
+import Row from 'react-bootstrap/Row';
+import { Helmet } from 'react-helmet-async';
+import MessageBox from '../components/MessageBox';
+import { Store } from '../Store';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export default function CartScreen() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  return (
+    <div>
+      <Helmet>
+        <title>Shopping Cart</title>
+      </Helmet>
+      <h1>Shopping Cart</h1>
+      <Row>
+        <Col md={8}>
+          {cardItems.length === 0 ? (
+            <MessageBox>
+              Cart is empty. <Link to="/">Go Shopping</Link>
+            </MessageBox>
+          ) : (
+            <ListGroup>
+              {cartItems.map((item) => (
+                <ListGroupItem key={item._id}>
+                  <Row classname="align-items-center">
+                    <Col md={4}>
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="img-fluid rounded img-thumbnail"
+                      ></img>{' '}
+                      <Link to={`/products/${item.slug}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={3}>
+                      <Button variant="light" disabled={item.quantity === 1}>
+                        <i className="fas fa-minus-circle"></i>
+                      </Button>{' '}
+                      <span>{item.quantity}</span>{' '}
+                      <Button
+                        variant="light"
+                        disabled={item.quantity === item.countInStock}
+                      >
+                        <i className="fas fa-plus-circle"></i>
+                      </Button>{' '}
+                    </Col>
+                    <Col md={3}>${item.price}</Col>
+                    <Col md={2}>
+                      <Button variant="light">
+                        <i className="fas fa-trash"></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          )}
+        </Col>
+        <Col md={4}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <h3>
+                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
+                  items) : $
+                  {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+                </h3>
+                <ListGroup.Item>
+                  <div className="d-grid">
+                    <Button
+                      type="button"
+                      variant="primary"
+                      disabled={cartItems.length === 0}
+                    >
+                      Comprar Ahora
+                    </Button>
+                  </div>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
