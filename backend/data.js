b873a0bf8571dbbf267c71a3faf669f8e7d48fe7
santiclfriedrich <santiclear1@hotmail.com>
2022-10-31 19:07:31 -0300

    Sample Products/Users

--- backend/data.js
+++ backend/data.js
@@ -1,50 +1,68 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
-    products: [
-        {
-            _id: '1',
-            name: 'Remera Negra',
-            slug: 'remera-negra-pernam',
-            category: 'Remeras',
-            image: '/images/remera-black.png',
-            price: 3700,
-            countInStock: 7,
-            brand: 'Pernambuco',
-            description: 'Remera negra lisa 100% algodon'
-        },
-        {
-            _id: '2',
-            name: 'Gorra Marron',
-            slug: 'gorra-marron-ejx',
-            category: 'Gorras',
-            image: '/images/gorra-marron.png',
-            price: 2500,
-            countInStock: 0,
-            brand: 'Ejx',
-            description: 'Gorra Corderoy basta media 100% algodón. Talle único. Etiqueta de marca bordada en frente.'
-        },
-        { 
-            _id: '3',
-            name: 'Cargo Pants',
-            slug: 'cargo-azul-pernam',
-            category: 'Pantalones',
-            image: '/images/cargo-pants.png',
-            price: 6300,
-            countInStock: 2,
-            brand: 'Pernambuco',
-            description: 'Pantalon Cargo Azul. Tiro alto/ Regular. Composición: Algodón'
-        },
-        { 
-            _id: '4',
-            name: 'Medias Blancas',
-            slug: 'medias-blancas-pernam',
-            category: 'Accesorios',
-            image: '/images/medias-shop.png',
-            price: 1200,
-            countInStock: 15,
-            brand: 'Pernambuco',
-            description: 'Medias blancas con rayas negras. Composición: Algodón'
-        },
-    ],
+  users: [
+    {
+      name: 'Santi',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Peter',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      //_id: '1',
+      name: 'Remera Negra',
+      slug: 'remera-negra-pernam',
+      category: 'Remeras',
+      image: '/images/remera-black.png',
+      price: 3700,
+      countInStock: 7,
+      brand: 'Pernambuco',
+      description: 'Remera negra lisa 100% algodon',
+    },
+    {
+      //_id: '2',
+      name: 'Gorra Marron',
+      slug: 'gorra-marron-ejx',
+      category: 'Gorras',
+      image: '/images/gorra-marron.png',
+      price: 2500,
+      countInStock: 0,
+      brand: 'Ejx',
+      description:
+        'Gorra Corderoy basta media 100% algodón. Talle único. Etiqueta de marca bordada en frente.',
+    },
+    {
+      //_id: '3',
+      name: 'Cargo Pants',
+      slug: 'cargo-azul-pernam',
+      category: 'Pantalones',
+      image: '/images/cargo-pants.png',
+      price: 6300,
+      countInStock: 2,
+      brand: 'Pernambuco',
+      description:
+        'Pantalon Cargo Azul. Tiro alto/ Regular. Composición: Algodón',
+    },
+    {
+      //_id: '4',
+      name: 'Medias Blancas',
+      slug: 'medias-blancas-pernam',
+      category: 'Accesorios',
+      image: '/images/medias-shop.png',
+      price: 1200,
+      countInStock: 15,
+      brand: 'Pernambuco',
+      description: 'Medias blancas con rayas negras. Composición: Algodón',
+    },
+  ],
 };
 
 export default data;
